db7adce44df889a16783d8b7719f84c7cc74d1e6
Lim Zhi Lit <g00440629@atu.ie>
2025-09-29 09:30:44 +0100

    Addition

---
+++ src/main/java/ie/atu/Calculator.java
@@ -0,0 +1,11 @@
+package ie.atu;
+
+public class Calculator {
+
+    public Calculator() {
+    }
+
+    public int add(int firstNum, int secondNum) {
+        return firstNum + secondNum;
+    }
+}
--- src/main/java/ie/atu/MainApp.java
+++ src/main/java/ie/atu/MainApp.java
@@ -1,5 +1,25 @@
 package ie.atu;
 
+import java.util.Scanner;
+
 public class MainApp {
 
+    public static void main(String[] args) {
+
+        //get user input
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Insert first number:");
+        int firstNum = sc.nextInt();
+        System.out.println("Insert second number:");
+        int secondNum = sc.nextInt();
+
+        //perform arithmatics
+        Calculator calculator = new Calculator();
+        int result = calculator.add(firstNum, secondNum);
+
+
+        // print result
+        System.out.println("The result is: " + result);
+
+    }
 }
