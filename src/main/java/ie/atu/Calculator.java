987ac826a11f46569d54d1ee4c1fc3d2a3c9b931
Lim Zhi Lit <g00440629@atu.ie>
2025-09-29 09:58:45 +0100

    subtraction branch

--- src/main/java/ie/atu/Calculator.java
+++ src/main/java/ie/atu/Calculator.java
@@ -8,4 +8,7 @@ public class Calculator {
     public int add(int firstNum, int secondNum) {
         return firstNum + secondNum;
     }
+
+    public int sub(int firstNum, int secondNum) {return firstNum - secondNum;}
+
 }
--- src/main/java/ie/atu/MainApp.java
+++ src/main/java/ie/atu/MainApp.java
@@ -6,20 +6,39 @@ public class MainApp {
 
     public static void main(String[] args) {
 
+        UserInput userInput = getUserInput();
+
+        //perform arithmatics
+        Calculator calculator = new Calculator();
+        double result = switch (userInput.operation) {
+            case "add" -> calculator.add(userInput.firstNum, userInput.secondNum);
+            case "sub" -> calculator.sub(userInput.firstNum, userInput.secondNum);
+            default -> throw new IllegalArgumentException("Invalid operation");
+
+        };
+        // print result
+        System.out.println("The result is: " + result);
+
+    }
+
+    public record UserInput(String operation, int firstNum, int secondNum) {}
+
+    public static UserInput getUserInput() {
         //get user input
         Scanner sc = new Scanner(System.in);
+        System.out.print("Choose the operation (add, subtract, multiply, divide): ");
+        String operation = sc.nextLine();
+        while (!operation.equals("add") && !operation.equals("subtract") && !operation.equals("multiply") && !operation.equals("divide")) {
+            System.out.print("Invalid input, ");
+            System.out.print("choose the operation (add, subtract, multiply, divide): ");
+            operation = sc.nextLine();
+        }
+
         System.out.println("Insert first number:");
         int firstNum = sc.nextInt();
         System.out.println("Insert second number:");
         int secondNum = sc.nextInt();
 
-        //perform arithmatics
-        Calculator calculator = new Calculator();
-        int result = calculator.add(firstNum, secondNum);
-
-
-        // print result
-        System.out.println("The result is: " + result);
-
+        return new UserInput(operation, firstNum, secondNum);
     }
 }
